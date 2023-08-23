2d964bcb412182706790f088c59506799561a751
Anton <anton.gill25@gmail.com>
2023-08-23 18:14:42 +0300

    App view structure
    Implemented base app pages structure routing and render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from game import Player, Game
 
 app = Flask(__name__)
 
@@ -6,7 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello_app'
+    return render_template('index.html')
+
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+
+@app.route('/game')
+def game_page():
+    return render_template('game.html')
 
 
 if __name__ == '__main__':
