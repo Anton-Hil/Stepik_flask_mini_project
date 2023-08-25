1ac9b0b29e53015e8a2c41ba246fac9116948b60
Anton <anton.gill25@gmail.com>
2023-08-25 19:05:18 +0300

    Settings
    Implemented SettingsForm to set required game parameters if necessary.
    
    Minor link structure reorganisation
    Now game resets after game_over page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from game import Player, Game, Enemy
-from forms import ChooseDirectionForm
+from forms import ChooseDirectionForm, SettingsForm
 from config import Config
 
 app = Flask(__name__)
@@ -10,14 +10,29 @@ app.config.from_object(Config)
 @app.route('/')
 @app.route('/index')
 def index():
-    game = Game()
-    game.populate_field()
+    Game()
     return render_template('index.html')
 
 
-@app.route('/settings')
+@app.route('/init')
+def initialise():
+    game = Game()
+    game.populate_field()
+    return redirect(url_for('game_page', turn_id=0))
+
+
+@app.route('/settings', methods=['get', 'post'])
 def settings():
-    return render_template('settings.html')
+    form = SettingsForm()
+    if request.method == 'GET':
+        return render_template('settings.html', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            game = Game()
+            game.update_parameters(form.field_height.data,
+                                   form.field_width.data,
+                                   form.difficulty.data)
+        return redirect(url_for('settings'))
 
 
 @app.route('/game/<int:turn_id>', methods=['get', 'post'])
@@ -34,9 +49,11 @@ def game_page(turn_id):
         if request.method == 'GET':
             return render_template('game.html', game=game, form=form)
         if request.method == 'POST':
-            direction = form.direction.data
-            move_status = game.move(Player, direction)
-            game.update_player_status()
+            move_status = 0
+            if form.validate_on_submit():
+                direction = form.direction.data
+                move_status = int(game.move(Player, direction))
+                game.update_player_status()
             return redirect(url_for('game_page', turn_id=move_status))
 
 
@@ -44,6 +61,13 @@ def game_page(turn_id):
 def game_over_page():
     player = Player()
     return render_template('game_over.html', player=player)
+
+
+@app.route('/clear')
+def clear():
+    game = Game()
+    game.reset_field()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
